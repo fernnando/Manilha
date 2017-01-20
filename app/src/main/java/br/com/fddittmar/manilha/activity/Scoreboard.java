2a0b2910bbe8ebaae4a1fc7b810ad480d40e55ea
fddittmar <fddittmar@gmail.com>
2017-01-20 17:43:46 -0300

    Criação da classe Scoreboard, atividade responsável pela tela do placar.

---
+++ app/src/main/java/br/com/fddittmar/manilha/activity/Scoreboard.java
@@ -0,0 +1,16 @@
+package br.com.fddittmar.manilha.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Fernnando on 20/01/2017.
+ */
+
+public class Scoreboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
