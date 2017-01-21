59afcd2940cdafa8aa14381813ab76868e6db9db
fddittmar <fddittmar@gmail.com>
2017-01-21 08:50:01 -0300

    Criação da interface da atividade inicial
    * Adição do logo do aplicativo
    * Adição dos botões: '4 jogadores', '6 jogadores' e 'Regras do truco'

--- app/src/main/java/br/com/fddittmar/manilha/activity/Scoreboard.java
+++ app/src/main/java/br/com/fddittmar/manilha/activity/Scoreboard.java
@@ -1,16 +1,15 @@
 package br.com.fddittmar.manilha.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Fernnando on 20/01/2017.
- */
+import br.com.fddittmar.manilha.R;
 
 public class Scoreboard extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoreboard);
     }
 }
